cc611686850e87361d5538ca1861e1cbb0cdfb79
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-13 20:50:24 -0300

    Add section component

---
+++ src/app/shared/section/section.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() title = '';
+
+  ngOnInit(): void {
+  }
+
+}
