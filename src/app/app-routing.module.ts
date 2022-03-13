fc094855089d37a727aeaa5e36d20ed31dc6adb7
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-13 20:54:55 -0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'portfolio', loadChildren: () => import('./portfolio/portfolio.module').then(m => m.PortfolioModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
