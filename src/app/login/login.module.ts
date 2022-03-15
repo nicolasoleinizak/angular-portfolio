a4a2d0f8e3ac0e2e57e93dcb747d28f21d174a4d
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-15 17:27:49 -0300

    Create login form view

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,14 +4,19 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    FormComponent,
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    ReactiveFormsModule,
+    LoginRoutingModule,
   ]
 })
 export class LoginModule { }
