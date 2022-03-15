4b8353a07fd795e9fb609f261b72dfac63646ad1
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-03-15 17:23:58 -0300

    Create component progress-bar

---
+++ src/app/shared/progress-bar/progress-bar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit {
+
+  constructor() {}
+
+  @Input() progress: number = 0;
+  @Input() height: number = 10;
+  @Input() color: string = 'rgba(0,0,0,0.8)';
+
+  containerStyle: object = {};
+  contentStyle: object = {};
+  
+  ngOnInit(): void {
+
+    console.log(this.progress)
+
+    this.containerStyle = {
+      'height': this.height+'px',
+      'border-radius': this.height/2,
+      'border-color': this.color
+    };
+
+    this.contentStyle = {
+      'width': this.progress+"%",
+      'height': this.height+'px',
+      'background': this.color
+
+    }
+  }
+
+}
